363bc8040e0ddf9b3cd82ffc22105dc2e276aba8
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-09 10:15:52 +0530

    nodemailer setup done

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -3,5 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT
-}
+  PORT: process.env.PORT,
+  SMPT_MAIL: process.env.SMPT_MAIL,
+  SMPT_PASSWORD: process.env.SMPT_PASSWORD,
+};
