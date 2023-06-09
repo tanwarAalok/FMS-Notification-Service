13db5ddbabc7fde7bad90b462710cf17f4a02978
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-09 10:50:13 +0530

    ticket create api created

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { InfoController } = require('../../controllers');
+const { InfoController, EmailController } = require('../../controllers');
 
 const router = express.Router();
 
 router.get('/info', InfoController.info);
+
+router.post('/ticket', EmailController.create);
 
 module.exports = router;
