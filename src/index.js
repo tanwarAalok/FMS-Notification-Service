c7f1b90444cac02fffd5be386e756824c6478269
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-09 20:43:36 +0530

    completed mail consumption event

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { ServerConfig } = require('./config');
+const { ServerConfig, Queue } = require('./config');
 const apiRoutes = require('./routes');
 
 const app = express();
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await Queue.connectQueue();
 });
